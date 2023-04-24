63224d903176c073fc9ee240b0eb25aea94eb07e
ravisingh8v <ravisingh8v@gmail.com>
2023-04-24 18:48:49 +0530

    feat:created hero and carousel

---
+++ src/component/home/HeroCarousel.vue
@@ -0,0 +1,103 @@
+<template>
+  <div
+    class="hero_carousel bg-secondary position-relative d-flex justify-content-center text-white"
+  >
+    <!-- transition the items  -->
+    <Transition mode="out-in" name="carousel">
+      <div
+        class="p-3 item_1 w-100 d-flex justify-content-center"
+        v-if="firstSlide"
+      >
+        <div class="">Daily Deal | Coffee & Pastry $3.99</div>
+      </div>
+      <div
+        class="p-3 item_2 w-100 d-flex justify-content-center"
+        v-else-if="secondSlide"
+      >
+        Weekend Brunch I Saturday 9am - 2pm
+      </div>
+    </Transition>
+    <!-- action buttons  -->
+    <a class="prev" @click="prev(count)">&lt;</a>
+    <a class="next" @click="next(count)">&gt;</a>
+  </div>
+</template>
+<script lang="ts">
+import { ref, watch } from "vue";
+export default {
+  setup() {
+    // data
+    const count = ref(1);
+    const firstSlide = ref();
+    const secondSlide = ref();
+    const thirdSlide = ref();
+
+    watch(
+      count,
+      () => {
+        if (count.value === 1) {
+          firstSlide.value = true;
+          secondSlide.value = false;
+        } else if (count.value === 2) {
+          secondSlide.value = true;
+          firstSlide.value = false;
+        }
+      },
+      { immediate: true }
+    );
+    setInterval(() => {
+      if (count.value == 2) {
+        count.value = 1;
+      } else {
+        count.value = 2;
+      }
+    }, 3000);
+    // function
+    function prev(value: number) {
+      if (count.value > 1) {
+        count.value = value - 1;
+      } else {
+        count.value = 2;
+      }
+    }
+    function next(value: number) {
+      if (count.value < 2) {
+        count.value = value + 1;
+      } else {
+        count.value = 1;
+      }
+      console.log(count.value);
+    }
+
+    return { count, prev, next, firstSlide, secondSlide, thirdSlide };
+  },
+};
+</script>
+<style lang="scss">
+.carousel-enter-from {
+  opacity: 0;
+  transform: translateX(480px);
+  // background-color: rgba(255, 255, 255, 0.863);
+}
+.carousel-leave-from {
+  opacity: 1;
+  background-color: unset;
+  transform: translateX(0px);
+}
+.carousel-enter-active {
+  transition: all 0.5s ease-out;
+}
+.carousel-leave-active {
+  transition: all 0.5s ease-in;
+}
+.carousel-enter-to {
+  opacity: 1;
+  background-color: unset;
+  transform: translateX(0px);
+}
+.carousel-leave-to {
+  opacity: 0;
+  transform: translateX(-480px);
+  // background-color: rgba(255, 255, 255, 0.863);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="home">
     <!-- hero wrapper  -->
-    <section class="hero-section theme-p">
+    <section class="hero-section ps-5 d-flex align-items-center text-white">
       <!-- hero content  -->
-      <div>
-        <span>Cafe & Restaurant Est.2035</span>
-        <div>
-          <h2>
+      <div class="ps-4">
+        <span class="d-inline-block mb-2 fs-4">Cafe & Restaurant Est.2035</span>
+        <div class="my-2">
+          <h2 class="d-flex flex-column">
             <span>Your Go-to Spot for</span>
             <span>Delicious Eats & Coffee</span>
           </h2>
         </div>
-        <div>
-          <button>Order Now</button>
+        <div class="my-4">
+          <button class="btn btn-danger">Order Now</button>
         </div>
       </div>
     </section>
+    <!-- carousel imported here  -->
+    <hero-carousel></hero-carousel>
+    <main class="home_main_wrapper">
+      <div class="row">
+        <div></div>
+        <div></div>
+      </div>
+      <div class="row">
+        <div></div>
+        <div></div>
+      </div>
+    </main>
+    <!-- footer section -->
+    <footer></footer>
   </div>
 </template>
 
 <script lang="ts">
+import HeroCarousel from "./../component/home/HeroCarousel.vue";
 import { defineComponent } from "vue";
-
-export default defineComponent({});
+export default defineComponent({
+  components: { HeroCarousel },
+});
 </script>
-<style lang="scss"></style>
